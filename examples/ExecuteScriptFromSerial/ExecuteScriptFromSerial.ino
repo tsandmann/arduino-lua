e8a3743e3da0c89043906bef979ab80322428105
Scotty Franzyshen <sfranzyshen@gmail.com>
2019-11-01 14:04:41 -0600

    Update ExecuteScriptFromSerial.ino
    strip builtins ... passoff to sketch

--- examples/ExecuteScriptFromSerial/ExecuteScriptFromSerial.ino
+++ examples/ExecuteScriptFromSerial/ExecuteScriptFromSerial.ino
@@ -2,7 +2,39 @@
 
 LuaWrapper lua;
 
+static int lua_wrapper_pinMode(lua_State *lua) {
+  int a = luaL_checkinteger(lua, 1);
+  int b = luaL_checkinteger(lua, 2);
+  pinMode(a, b);
+}
+
+static int lua_wrapper_digitalWrite(lua_State *lua) {
+  int a = luaL_checkinteger(lua, 1);
+  int b = luaL_checkinteger(lua, 2);
+  digitalWrite(a, b);
+}
+  
+static int lua_wrapper_delay(lua_State *lua) {
+  int a = luaL_checkinteger(lua, 1);
+  delay(a);
+}
+/*
+static int lua_wrapper_print(lua_State *lua) {
+  String a = String(luaL_checkstring(lua, 1));
+  Serial.println(a);
+}
+*/
+static int lua_wrapper_millis(lua_State *lua) {
+  lua_pushnumber(lua, (lua_Number) millis());
+  return 1;
+}
+
 void setup() {
+  lua.Lua_register("pinMode", (const lua_CFunction) &lua_wrapper_pinMoe);
+  lua.Lua_register("digitalWrite", (const lua_CFunction) &lua_wrapper_digitalWrite);
+  lua.Lua_register("delay", (const lua_CFunction) &lua_wrapper_delay);
+  //lua.Lua_register("print", (const lua_CFunction) &lua_wrapper_print);
+  lua.Lua_register("millis", (const lua_CFunction) &lua_wrapper_millis);
   Serial.begin(115200);
 }
 
