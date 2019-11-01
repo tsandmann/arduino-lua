4c20b7117ced00702a46596bf35389e155cbb15a
Scotty Franzyshen <sfranzyshen@gmail.com>
2019-11-01 13:43:04 -0600

    Update LuaWrapper.cpp
    strip builtins ... passoff to sketch

--- src/LuaWrapper.cpp
+++ src/LuaWrapper.cpp
@@ -1,41 +1,16 @@
 #include "LuaWrapper.h"
 
 extern "C" {
-  static int lua_wrapper_pinMode(lua_State *lua) {
-    int a = luaL_checkinteger(lua, 1);
-    int b = luaL_checkinteger(lua, 2);
-    pinMode(a, b);
-  }
-
-  static int lua_wrapper_digitalWrite(lua_State *lua) {
-    int a = luaL_checkinteger(lua, 1);
-    int b = luaL_checkinteger(lua, 2);
-    digitalWrite(a, b);
-  }
-  
-  static int lua_wrapper_delay(lua_State *lua) {
-    int a = luaL_checkinteger(lua, 1);
-    delay(a);
-  }
 
   static int lua_wrapper_print(lua_State *lua) {
     String a = String(luaL_checkstring(lua, 1));
     Serial.println(a);
   }
-
-  static int lua_wrapper_millis(lua_State *lua) {
-    lua_pushnumber(lua, (lua_Number) millis());
-    return 1;
-  }
 }
 
 LuaWrapper::LuaWrapper() {
   _state = luaL_newstate();
-  lua_register(_state, "pinMode", lua_wrapper_pinMode);
-  lua_register(_state, "digitalWrite", lua_wrapper_digitalWrite);
-  lua_register(_state, "delay", lua_wrapper_delay);
   lua_register(_state, "print", lua_wrapper_print);
-  lua_register(_state, "millis", lua_wrapper_millis);
 }
 
 String LuaWrapper::Lua_dostring(const String *script) {
