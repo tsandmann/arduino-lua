1dfdf0baeaf85a475e4f19a971262f7e8e848a69
Scotty Franzyshen <sfranzyshen@gmail.com>
2019-11-01 16:13:56 -0600

    Update HelloWorld.ino

--- examples/HelloWorld/HelloWorld.ino
+++ examples/HelloWorld/HelloWorld.ino
@@ -2,7 +2,13 @@
 
 LuaWrapper lua;
 
+static int lua_wrapper_print(lua_State *lua_state) {
+  String a = String(luaL_checkstring(lua_state, 1));
+  Serial.println(a);
+}
+
 void setup() {
+  lua.Lua_register("print", (const lua_CFunction) &lua_wrapper_print);
   Serial.begin(115200);
   String script = String("print('Hello world!')");
   Serial.println(lua.Lua_dostring(&script));
