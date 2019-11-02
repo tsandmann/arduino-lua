2838d60764b6281993390716f6fd97995bbd6895
Scotty Franzyshen <sfranzyshen@gmail.com>
2019-11-02 10:23:27 -0600

    Update LuaWrapper.cpp
    Added import for lua standard libraries:
    
    - base (tostring(), `tonumber(), etc.)
    - math
    - table (insert(), sort(), remove(), ...)
    - string (len(), match(), ...)
    
    Replaced print() function with the original version modified with Serial

--- src/LuaWrapper.cpp
+++ src/LuaWrapper.cpp
@@ -1,7 +1,35 @@
 #include "LuaWrapper.h"
 
+extern "C" {
+  static int lua_wrapper_print (lua_State *L) {
+    int n = lua_gettop(L);  /* number of arguments */
+    int i;
+    lua_getglobal(L, "tostring");
+    for (i=1; i<=n; i++) {
+      const char *s;
+      size_t l;
+      lua_pushvalue(L, -1);  /* function to be called */
+      lua_pushvalue(L, i);   /* value to print */
+      lua_call(L, 1, 1);
+      s = lua_tolstring(L, -1, &l);  /* get result */
+      if (s == NULL)
+        return luaL_error(L, "'tostring' must return a string to 'print'");
+      if (i>1) Serial.write("\t");
+      Serial.write(s);
+      lua_pop(L, 1);  /* pop result */
+    }
+    Serial.println();
+    return 0;
+  }
+} 
+
 LuaWrapper::LuaWrapper() {
   _state = luaL_newstate();
+  luaopen_base(_state);
+  luaopen_table(_state);
+  luaopen_string(_state);
+  luaopen_math(_state);
+  lua_register(_state, "print", lua_wrapper_print);
 }
 
 String LuaWrapper::Lua_dostring(const String *script) {
